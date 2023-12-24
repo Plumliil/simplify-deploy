0f91c0dcf1632ecb405eaab7d560ed2bc50a4051
plumliil <plumliil@foxmail.com>
2023-12-24 14:35:47 +0800

    feat: select Environment to list all options when publishing

--- utils/configHandle.js
+++ utils/configHandle.js
@@ -22,13 +22,15 @@ function optionsCheck(config) {
       return false;
     }
   }
-  // 检查嵌套的dev、uat、prod对象
-  for (const key of ['dev', 'uat', 'prod']) {
+  const targetEnv = Object.keys(config)?.filter(item => {
+    return typeof config[item] === 'object'
+  })
+  // 检查嵌套的环境对象
+  for (const key of targetEnv) {
     const nestedObject = config[key];
     if (nestedObject && typeof nestedObject === 'object') {
       // 检查嵌套对象是否包含必需的嵌套属性
       const requiredNestedProperties = ['remoteDir', 'localDir'];
-
       for (const nestedProperty of requiredNestedProperties) {
         if (!(nestedProperty in nestedObject)) {
           console.log(`错误：环境属性 '${nestedProperty}' 不存在于 ${key} 配置对象中。`)
@@ -58,7 +60,9 @@ function getConf(types = [], config = []) {
   if (option) {
     config = option;
     // 过滤出有效的类型
-    types = Object.keys(option).filter(item => ['dev', 'uat', 'prod'].includes(item));
+    types = Object.keys(option)?.filter(item => {
+      return typeof config[item] === 'object'
+    })
   }
   // 返回有效的类型和配置信息
   return { types, config };
